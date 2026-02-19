168e086b371775639282a66f7c4aaca9e44d5bff
Divyansh-techsource <dk.tech2007@gmail.com>
2026-02-19 14:59:10 +0530

    19-02-2026

--- basicecommerce/src/App.jsx
+++ basicecommerce/src/App.jsx
@@ -34,8 +34,8 @@ function App() {
   return (
     <>
     <Header cart={cart.length}>
-    <ProductList/>
-    <Cart/>
+    {/* <ProductList/>
+    <Cart/> */}
     </>
   );
 }
--- basicecommerce/src/Components/Header.jsx
+++ basicecommerce/src/Components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header({cartCount}){
     return(
         <>
         <h2>React Demo App</h2>
-
+        <p>Cart Item:{cartCount}</p>
         </>
     )
 }
