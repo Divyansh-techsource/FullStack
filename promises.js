09145ab9388ed754b21a03b8e4747ba21f6fb492
Divyansh-techsource <dk.tech2007@gmail.com>
2026-03-16 16:18:15 +0530

    16-03-2026

---
+++ promises.js
@@ -0,0 +1,14 @@
+let p=new Promise((resolve,reject)=>{
+    let s=true;
+    if(s){
+        resolve("Data Connected");
+    }
+    else{
+        reject("error in communication");
+    }
+});
+//consuming promise
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(response=>response.json())
+.then(data=>console.log(data))
+.catch(error=>console.log(error))
