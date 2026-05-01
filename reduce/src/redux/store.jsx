da63a8b0ebf05cd54e044b3fd44627b22ace6e94
Divyansh Kaushik <dk.tech2007@gmail.com>
2026-05-01 16:41:59 +0530

    Redux Commits

--- reduce/src/redux/store.jsx
+++ reduce/src/redux/store.jsx
@@ -2,4 +2,9 @@ import { createStore } from 'redux';
 
 import { todoReducer } from './reducer';
 
-export const store = createStore(todoReducer);
+export const store = configureStore(
+    {
+        reducer:
+        {todo:todoreducer}
+    }
+);
