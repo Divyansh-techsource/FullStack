adfe39f51451b9cb8d55233042016e5ecc8b383e
Divyansh Kaushik <dk.tech2007@gmail.com>
2026-05-05 16:24:45 +0530

    05-05-2026

---
+++ NEXTJS/studentapp/app/settings/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div>
+      <h3> We are here to help</h3>
+      <p>This msg is created to explore next</p>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
