91bd623fbb82ece3b678d3071c7977ead33f8753
Divyansh Kaushik <dk.tech2007@gmail.com>
2026-05-06 16:14:54 +0530

    next-routing

---
+++ NEXTJS/studentapp/app/login/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import {useRouter} from "next/navigation";
+
+export default function LoginPage(){
+    return(
+        <div>
+            <h1>Login Page</h1>
+            
+        </div>
+    )
+}
